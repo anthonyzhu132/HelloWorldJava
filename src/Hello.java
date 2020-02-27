01b3b5608b510b6c59b3c62755b53d455f42bcd9
Anthony Zhu <anthonyzhu132@gmail.com>
2020-02-26 21:49:43 -0800

    added initial block code to display hello world

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int myFirstNumber = 5; //Declaring a variable of type INT with the name of myFirstNumber with a value of 5
     }
 }
